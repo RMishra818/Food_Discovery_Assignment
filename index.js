ad7e398d5ee930c89fdbd1f28d9393955e44f8ec
RMishra818 <mishraraghav736@gmail.com>
2024-11-16 19:13:04 +0530

    Added promise to handle db all

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 app.use(cors());
-app.use(express.static('static'));
 const db = new sqlite3.Database('./database.sqlite', (err) => {
   if (err) {
     console.error('Error opening database:', err.message);
@@ -13,16 +12,21 @@ const db = new sqlite3.Database('./database.sqlite', (err) => {
     console.log('Connected to the SQLite database.');
   }
 });
-async function getAllRestaurants() {
-  let query = 'Select * From Restaurants';
-  let result = await db.all(query, []);
-  return result;
+function getAllRestaurants() {
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT * FROM restaurants';
+    db.all(query, [], (err, rows) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(rows); 
+    });
+  });
 }
 
-app.get('/restaurants', async (req, res) => {
+app.get('/restaurants', async(req, res) => {
   try {
     let result = await getAllRestaurants();
-    console.log(result);
     if (result == null) {
       res.status(404).json('No restaurants available');
     }
